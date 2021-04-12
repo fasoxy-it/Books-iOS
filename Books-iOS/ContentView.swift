2abe60d0814e6fd5b1099190a4c6fe6fbcaca2ed
fasoxy-it <fasoxy@gmail.com>
2021-04-12 17:01:17 +0200

    New Commit

--- Books-iOS/ContentView.swift
+++ Books-iOS/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var books: [Book] = [
+        Book.init(title: "Clean Code", link: "https://amzn.to/2YqSuEu", image: UIImage(imageLiteralResourceName: "cleancode")),
+        Book.init(title: "Elegant Objects", link: "https://amzn.to/32N4dgB", image: UIImage(imageLiteralResourceName: "elegantobjects")),
+        Book.init(title: "Design Patterns", link: "https://amzn.to/2YkHCZ2", image: UIImage(imageLiteralResourceName: "designpatterns"))
+    ]
+    
+    @State var searchIsPresented: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack (alignment: .leading) {
+                List(
+                    books,
+                    id: \.title,
+                    rowContent: { book in
+                    
+                        NavigationLink(
+                            destination: Text(book.title),
+                            label: {
+                                Image(uiImage: book.image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 120, height: 160)
+                                    .cornerRadius(8)
+                                    .shadow(radius: 20)
+                                    .padding([.top, .bottom])
+                                
+                                VStack (alignment: .leading) {
+                                    Text(book.title)
+                                        .fontWeight(.bold)
+                                    Text(book.link)
+                                        .font(.callout)
+                                }
+                            })
+                        
+                })
+            }
+            .navigationBarTitle("Programming Books")
+            .navigationBarItems(trailing: Button(action: {
+                self.searchIsPresented.toggle()
+            }, label: {Text("Search")}))
+        }
+        .sheet(isPresented: $searchIsPresented, content: {
+            VStack {
+                Text("Search View")
+                Spacer()
+                Button(action: {
+                    self.searchIsPresented.toggle()
+                }, label: {
+                    Text("Close")
+                })
+            }
             .padding()
+        })
     }
 }
 
---
+++ Books-iOS/Model/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  Books-iOS
+//
+//  Created by Mattia Fasoli on 12/04/21.
+//
+
+import Foundation
+import UIKit
+
+struct Book {
+    let title: String
+    let link: String
+    let image: UIImage
+}
