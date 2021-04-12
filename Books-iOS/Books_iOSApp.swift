bff58424aa445571a11a0f4963c93dff0b12bac6
fasoxy-it <fasoxy@gmail.com>
2021-04-12 15:55:47 +0200

    Initial Commit

---
+++ Books-iOS/Books_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Books_iOSApp.swift
+//  Books-iOS
+//
+//  Created by Mattia Fasoli on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Books_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
